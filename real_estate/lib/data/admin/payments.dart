2c001b78020746c7d588ea4167fcc17d4338869a
Paul Sho <Cyberkiller.CK@gmail.com>
2025-03-20 19:07:19 +0800

    Add payment addition functionality and update CMake build files

--- real_estate/lib/data/admin/payments.dart
+++ real_estate/lib/data/admin/payments.dart
@@ -31,3 +31,56 @@ Future<List<dynamic>?> getPayments() async {
   }
   return null;
 }
+
+Future<void> addPayment(
+  BuildContext context,
+  String firstname,
+  String middlename,
+  String lastname,
+  String email,
+  String contact,
+  String? house_id,
+) async {
+  Links link = Links();
+
+  final Map<String, dynamic> jsonData = {};
+
+  final Map<String, String> queryParams = {
+    "operation": "addPayment",
+    "json": jsonEncode(jsonData),
+  };
+
+  try {
+    final response = await http.post(
+      Uri.parse(link.admin_link).replace(queryParameters: queryParams),
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final res = jsonDecode(response.body);
+
+      if (res == "0") {
+        print("Something went wrong");
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Failed to add payment")));
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Payment added successfully!")));
+        getPayments();
+      }
+    } else {
+      print("Server Error: ${response.statusCode}");
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error adding payment")));
+    }
+  } catch (error) {
+    print("Runtime Error: $error");
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Network error while adding payment")),
+    );
+  }
+}
